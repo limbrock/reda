689d37fec5de257d6c8bbd9a1d6635835ea50c9f
Travis CI User <travis@example.org>
2018-01-31 07:42:09 +0000

    [by travis] update documentation

---
+++ documentation/api/reda-plotters-2.py
@@ -0,0 +1,8 @@
+# Missing dependency on travis
+# import crtomo.configManager as CRconfig
+# config = CRconfig.ConfigManager(nr_of_electrodes=48)
+# config.gen_dipole_dipole(skipc=2)
+# fig, ax = config.plot_pseudodepths(
+#     spacing=0.3,
+#     ctypes=['dd', ],
+# )
---
+++ documentation/api/reda-plotters-3.py
@@ -0,0 +1,7 @@
+# import crtomo.configManager as CRconfig
+# config = CRconfig.ConfigManager(nr_of_electrodes=48)
+# config.gen_schlumberger(M=24, N=25)
+# fig, ax = config.plot_pseudodepths(
+#     spacing=1,
+#     ctypes=['schlumberger', ],
+# )
